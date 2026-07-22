3fc51f07988ca27947056aba6121291292274640
renuka2204 <renukashinge14@gmail.com>
2026-07-22 23:29:42 +0530

    session-2 update

---
+++ Day-2/class.js
@@ -0,0 +1,68 @@
+// Hoisting:-
+
+var number = 20;
+console.log(number);
+
+
+{
+  userName = "Renuka";
+  console.log(userName);
+
+  var userName;
+}
+
+console.log(userAge)
+var userAge = 25;
+
+let userData = null;
+console.log(userData)
+
+
+// Arithmetic Operators
+
+console.log(1 + 2);
+console.log(1 - 2);
+console.log(1 * 2);
+console.log(1 / 2);
+
+console.log(1 % 2);
+console.log(2 % 2);
+console.log(3 % 2);
+console.log(4 % 2);
+console.log(5 % 2);
+console.log(6 % 2);
+console.log(7 % 2);
+console.log(8 % 2);
+console.log(9 % 2);
+console.log(10 % 2);
+console.log(11 % 2);
+
+
+// Comparison Operators
+
+console.log(2 > 1);
+console.log(2 < 1);
+
+console.log(2 >= 2)
+console.log(2 <= 2 )
+
+console.log(2 != 3)
+console.log(2 != 2)
+
+console.log(2 == 4);
+console.log(2 == 2);
+
+console.log(2 == "2");
+console.log(2 === "2");
+
+
+//Logical Operators 
+
+console.log(2 == 2 && 4 == 4 && 4 == 4);
+// // && all conditions must true then only itll return true
+// // if any single condition is wrong then itll return false
+
+
+console.log(2 == 1 || 3 == 1 || 4 == 1);
+// // || if any single condition is true then itll return true
+// // when all conditions are false/ wrong then only itll return false
