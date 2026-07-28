f92b554d1f336cf1128decc70653635bd54e26f5
renuka2204 <renukashinge14@gmail.com>
2026-07-28 16:31:33 +0530

    update

---
+++ Day-3/class.js
@@ -0,0 +1,73 @@
+// Funcation
+
+function greet(name) {
+    console.log("hello" + name)
+}
+greet("Renuka")
+
+
+function addNumbers (num1, num2){
+    console.log(22,22)
+}
+addNumbers()
+
+
+function introducation (name,age,city){
+    console.log(name)
+    console.log(age)
+    console.log(city)
+}
+introducation("Renuka",22,"solapur")
+
+
+function say(){
+    console.log("End")
+}
+console.log("Start");
+console.log("Hii");
+say()
+
+
+//multiple
+function data(name){
+    console.log("hello")
+}
+data("Renuka");
+data("Puja");
+data("Sneha");
+data("Sayali");
+
+function data(name){
+    console.log(name)
+}
+data("Renuka");
+data("Puja");
+data("Sneha");
+data("Sayali");
+
+
+//Arithmetic
+function add(a,b){
+    console.log (a+b);
+    console.log (a-b);
+    console.log (a*b);
+    console.log (a%b);
+}
+add(10,10)
+
+const hello = () =>{
+    console.log("Hello")
+}
+hello();
+
+const add = (a, b) => {
+    console.log(a * b);
+    console.log(a + b);
+    console.log(a - b);
+    console.log(a % b);
+}
+add(10, 20);
+
+
+
+
