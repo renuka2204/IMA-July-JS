5478b4ffc445ab5641b59f522c18f601140f2f94
renuka2204 <renukashinge14@gmail.com>
2026-09-02 22:16:36 +0530

    Update

---
+++ Day-7/class.js
@@ -0,0 +1,34 @@
+// // Function to add two numbers
+function add(a, b){
+    return a + b;
+}
+const result = add(20, 30);
+console.log(result);
+
+// // Function to subtract two numbers
+function sub(a, b){
+    return a - b;
+}
+const result1 = sub(10, 5);
+console.log(result1);
+
+// // Function to multiply two numbers
+function mult(a, b){
+    return a * b;
+}
+const result2 = mult(10, 15);
+console.log(result2);
+
+// // Function to divide two numbers
+function div(a, b){
+    return a / b;
+}
+const result3 = div(10, 5);
+console.log(result3);
+
+// Function to calculate the square of a number
+function square(num){
+    return num * num;
+}
+const result4 = square(5);
+console.log(result4);
